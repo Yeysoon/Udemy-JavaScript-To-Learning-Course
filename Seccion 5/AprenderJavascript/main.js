d402712ba21adddd08aea331d178ae87f87c6cd2
Yeysoon <yeysonaldair13@gmail.com>
2025-12-28 13:02:34 -0600

    Un scrip aparte.

---
+++ Seccion 5/AprenderJavascript/main.js
@@ -0,0 +1,6 @@
+alert("Hola que tal soy un mensaje bienvenido al curso");
+alert("Estamos aprendiendo JavaScript");
+console.log("Estamos aprendiendo JavaScript");
+
+console.log("Hola que tal soy un mensaje bienvenido al curso");
+console.log(123 + 2);
