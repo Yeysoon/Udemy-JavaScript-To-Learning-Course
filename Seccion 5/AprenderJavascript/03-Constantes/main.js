bc919702b730522fffaac5c5baf695a3cfbc2a68
Yeysoon <yeysonaldair13@gmail.com>
2025-12-29 00:13:29 -0600

    variables 3 const

---
+++ Seccion 5/AprenderJavascript/03-Constantes/main.js
@@ -0,0 +1,13 @@
+//Constante
+//Es un varible cuyo contenido no puede variar, nunca jamas de los jamases cambia.
+
+let nombre = "Yeyson";
+let web = "yeysonbarillas.com";
+
+
+const dpi = "3238017440511";
+
+nombre = "Juan";
+dpi = "73483673486734867";
+
+console.log(nombre, dpi);
