dbe08c974315dcccedd7e833b2c79c83513f1ed3
Yeysoon <yeysonaldair13@gmail.com>
2025-12-28 23:55:17 -0600

    variables

---
+++ Seccion 5/AprenderJavascript/02-Variables/script.js
@@ -0,0 +1,11 @@
+//Variables
+//Es una caja donde guardo una informaion.
+
+//Var tiene un alcance mas amplio es la manera clasica de declarar variables
+var nombre = "Yeyson";
+
+//Let tiene mejores practicas que con var
+let apellido = "Barillas";
+
+//Const esun variable que su valor no puede ser modificado 
+const nacionalidad = "Guatemalteco";
