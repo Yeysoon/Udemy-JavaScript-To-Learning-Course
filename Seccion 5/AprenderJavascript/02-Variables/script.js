83f3f367a56316e09a704e1c4d4d46a4c366567c
Yeysoon <yeysonaldair13@gmail.com>
2025-12-29 00:05:09 -0600

    variables 2.

--- Seccion 5/AprenderJavascript/02-Variables/script.js
+++ Seccion 5/AprenderJavascript/02-Variables/script.js
@@ -1,6 +1,6 @@
 //Variables
 //Es una caja donde guardo una informaion.
-
+/*
 //Var tiene un alcance mas amplio es la manera clasica de declarar variables
 var nombre = "Yeyson";
 
@@ -9,3 +9,18 @@ let apellido = "Barillas";
 
 //Const esun variable que su valor no puede ser modificado 
 const nacionalidad = "Guatemalteco";
+
+*/
+
+let heroe = "Spiderman";
+let universo = "Marvel";
+let edad = 25;
+let heroe_y_universo = heroe + " es un super heroe de la marca " + universo;
+
+heroe = "Batman";
+universo = "DC";
+console.log(heroe);
+console.log(universo);
+alert(heroe_y_universo);
+
+console.log(edad, heroe, universo);
