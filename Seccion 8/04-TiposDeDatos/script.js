5f3287bbee8ab5c3c3ef35d9b3e456265d781eba
Yeysoon <yeysonaldair13@gmail.com>
2025-12-29 23:24:54 -0600

    tipos de datos

---
+++ Seccion 8/04-TiposDeDatos/script.js
@@ -0,0 +1,10 @@
+let frase = "Tipos de datos en JavaScript";         //String cadena de texto
+let anio = 2025;                                  //Number numero int
+let interes = 2.5;                                //Numero decimal float /double  
+let mayorEdad = true;                             //tipo de dato Booleano true o false
+let vacia;                                        //valor undefined
+let nula = null;                                  //valor null
+let fruta = ["Manzana", "Banana", "Mango"];             //tipo de dato Array arreglo
+let persona = { nombre: "Miguel", universo: "DC" };    //tipo de dato Object JSON
+
+console.log(persona.nombre);
