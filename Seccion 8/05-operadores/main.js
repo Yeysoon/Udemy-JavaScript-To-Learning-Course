18ac53b023a6e06fe3dea88124c62f0716bcf493
Yeysoon <yeysonaldair13@gmail.com>
2025-12-29 23:32:03 -0600

    operadores

---
+++ Seccion 8/05-operadores/main.js
@@ -0,0 +1,28 @@
+//Operadores de tipo typeof
+
+let nombre = "Miguel";
+let edad = 25;
+let precio = 25.5;
+let mayorEdad = true;
+let vacia;
+let nula = null;
+let fruta = ["Manzana", "Banana", "Mango"];
+let persona = { nombre: "Miguel", universo: "DC" };
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof precio);
+console.log(typeof mayorEdad);
+console.log(typeof vacia);
+console.log(typeof nula);
+console.log(Array.isArray(fruta));
+console.log(typeof persona);
+
+
+//operadores aritmeticos o matematicos
+
+//operadores de asignacion
+//operadores de comparacion
+//operadores logicos
+//operadores de cadena
+//operadores de incremento y decremento 
