2400d4492787cb32f63c0c968e01c1377916e46b
Yeysoon <yeysonaldair13@gmail.com>
2026-01-04 13:21:38 -0600

    ejercicio 6

--- Seccion 8, 9, 10/06-Ejercicios/ejercicio06.js
+++ Seccion 8, 9, 10/06-Ejercicios/ejercicio06.js
@@ -1,1 +1,17 @@
-// Ejercicio 06
+/*
+Ejercicio 06:
+
+En mi tienda de videojuegos tenemos una oferta.
+
+Si compras un juego de 50 euros, ten hacemos un 20% de descuento.
+
+Si un cliente compra el Tekken 15 que cuesta 50 euros, ¿en cuanto se le queda?
+
+*/
+
+const compraCliente = 50;
+const descuento = 20;
+let descuentoCliente = compraCliente * descuento / 100;
+let totalAPagar = compraCliente - descuentoCliente;
+console.log("El cliente compro un total de " + compraCliente + " euros y como cumple con la oferta de la tienda se le hara un descuento del" + descuento + "%, por lo que el total a pagar es " + totalAPagar + " euros");
+
