75bb9d30d7ed294c3ef15cff89335c2b4c1d9e26
Yeysoon <yeysonaldair13@gmail.com>
2026-01-04 12:48:44 -0600

    ejercicio 2

--- Seccion 8, 9, 10/06-Ejercicios/ejercicio02.js
+++ Seccion 8, 9, 10/06-Ejercicios/ejercicio02.js
@@ -1,1 +1,15 @@
-// Ejercicio 02
+/*
+Ejercicio 2:
+
+Tenemos 7 cajas con 8 cookies rellenas de chocolate cada una. 
+
+¿Cuantas cookies en total tenemos en realidad?
+
+*/
+
+let cajas = 7;
+let cookiesxCaja = 8;
+
+let totalCookies = cajas * cookiesxCaja;
+
+console.log("El total de cookies que tenemos es de " + totalCookies + "por que en cada caja hay " + cookiesxCaja + " cookies y tenemos " + cajas + " cajas");
