8357d344d59bc1435afff19e18c69f3ec3c282bd
Yeysoon <yeysonaldair13@gmail.com>
2026-01-04 12:56:48 -0600

    ejercicio 3

--- Seccion 8, 9, 10/06-Ejercicios/ejercicio03.js
+++ Seccion 8, 9, 10/06-Ejercicios/ejercicio03.js
@@ -1,1 +1,17 @@
-// Ejercicio 03
+/*
+Ejercicio 03:
+
+¿Cuantos minutos tienen 7 horas?
+
+Utiliza varibables  y operadores para resolverlo.
+
+*/
+
+const horas = 7;
+let hora = 1
+let minutos = 60;
+
+let totalMinutoshora = hora * minutos;
+let totalMinutosTotales = totalMinutoshora * horas;
+
+console.log("Los minutos que tienen " + horas + " horas es: " + totalMinutosTotales + " minutos");   
