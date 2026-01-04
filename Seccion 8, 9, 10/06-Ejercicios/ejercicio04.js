31c3be77f88a27fe45209b66d2ddbce41eb1f882
Yeysoon <yeysonaldair13@gmail.com>
2026-01-04 13:01:59 -0600

    ejericio 4

--- Seccion 8, 9, 10/06-Ejercicios/ejercicio04.js
+++ Seccion 8, 9, 10/06-Ejercicios/ejercicio04.js
@@ -1,1 +1,15 @@
-// Ejercicio 04
+/*
+Ejercicio 04:
+
+Tenemos una jirafa en el zoologico que pesa 1.120 kg.
+y le damos de comer 141 kilos de hojas frescas
+
+¿Cuanto pesa ahora la jirafa?
+
+*/
+
+let pesoDeJirafa = 1120;
+let comidaDeJirafa = 141;
+const pesoTotalJirafa = pesoDeJirafa + comidaDeJirafa;
+
+console.log("El peso de la jirafa ahora es de " + pesoTotalJirafa + " kilos")
