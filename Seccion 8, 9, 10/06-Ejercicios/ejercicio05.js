93768ccb589e939e7a509a26e96ebafc28f95e9a
Yeysoon <yeysonaldair13@gmail.com>
2026-01-04 13:13:55 -0600

    ejercicio 6

--- Seccion 8, 9, 10/06-Ejercicios/ejercicio05.js
+++ Seccion 8, 9, 10/06-Ejercicios/ejercicio05.js
@@ -1,1 +1,17 @@
-// Ejercicio 05
+/*
+Ejercicio 05:
+
+Tengo una tarta de queso con 16 porciones
+
+Cada porcion vale 3.8 euros.
+
+¿Si vendo todas las porciones, cuanto dinero gano en total?
+
+*/
+
+let precioPorPorcion = 3.8;
+let porcionesXTarta = 16;
+
+let totalGanado = precioPorPorcion * porcionesXTarta;
+
+console.log("El total ganado es: " + totalGanado + " euros");
