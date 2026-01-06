a44e985b32b17e7be35308a2f32fbdec091b9710
Yeysoon <yeysonaldair13@gmail.com>
2026-01-05 23:47:27 -0600

    ejercicio 7

--- Seccion 8, 9, 10/06-Ejercicios/ejercicio07.js
+++ Seccion 8, 9, 10/06-Ejercicios/ejercicio07.js
@@ -1,1 +1,11 @@
-// Ejercicio 07
+/*
+Ejercicio 07:
+
+Tenemos un circulo con un diametro d 18 cm.
+
+Calcula el radio del circulo y dime cual es en una alerta.
+*/
+
+const diametro = 18;
+const radio = diametro / 2;
+alert("El radio del circulo es: " + radio + " cm");
