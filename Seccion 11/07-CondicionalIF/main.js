8c4bc681435ff8668b3f4044bc6a5ae08a82d1df
Yeysoon <yeysonaldair13@gmail.com>
2026-01-08 22:13:46 -0600

    Condicional if y elseif

--- Seccion 11/07-CondicionalIF/main.js
+++ Seccion 11/07-CondicionalIF/main.js
@@ -19,3 +19,37 @@ if (estaLloviendo == true) {
     //si no se cumple la condicion se ejecuta este bloque de codigo
     console.log("No lleva paraguas");
 }
+
+//Ejemplo 2:
+
+let quieroCebolla = true;
+
+if (quieroCebolla == true) {
+    console.log("tu hamburgesa llevara cebolla");
+} else {
+    console.log("tu hamburgesa no llevara cebolla");
+}
+
+
+//Ejemplo 3:
+
+let nombre2 = "Joaquin Perez";
+let edad2 = 17;
+
+if (edad2 >= 18) {
+    // Es mayor de edad
+    console.log(nombre2 + " tiene " + edad2 + " años y es mayor de edad");
+
+    if (edad2 <= 20) {
+        console.log("Es un adolescente");
+
+    } else if (edad2 >= 70) {
+        console.log("Es un anciano");
+
+    } else {
+        console.log("Es un adulto");
+    }
+
+} else {
+    console.log(nombre2 + " tiene " + edad2 + " años y es menor de edad");
+}
