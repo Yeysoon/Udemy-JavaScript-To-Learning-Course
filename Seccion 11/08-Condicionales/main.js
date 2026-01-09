1b9532544dd642feb989ee76864aa3f55c94e2bd
Yeysoon <yeysonaldair13@gmail.com>
2026-01-08 23:46:26 -0600

    condicional switch

---
+++ Seccion 11/08-Condicionales/main.js
@@ -0,0 +1,22 @@
+//Condicionales Switch
+
+let miDesayuno = "huevos";
+
+switch (miDesayuno) {
+    case "tortitas":
+        console.log("Has seleccionado el desayuno de tortitas de cirope de arce.");
+        break;
+    case "huevos":
+        console.log("Has seleccionado el desayuno de huevos fritos con bacon.");
+        break;
+    case "pan":
+        console.log("Has seleccionado el desayuno de pan con queso.");
+        break;
+    default:
+        console.log("Has elegido otro desayuno");
+}
+
+//Condicional Ternario
+
+
+//Diferencias entre var y let (ambito/bloque)
