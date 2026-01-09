3579de66c352907c554600b5c76683b422806c77
Yeysoon <yeysonaldair13@gmail.com>
2026-01-08 22:03:36 -0600

    condicional if

---
+++ Seccion 11/07-CondicionalIF/main.js
@@ -0,0 +1,21 @@
+/* Condicional IF
+
+Permite tomar una decision
+
+Por ejemplo si A es igual a B entonces haz algo.
+
+Ejemplo 1: 
+
+
+*/
+
+let estaLloviendo = true;
+
+if (estaLloviendo == true) {
+
+    //si se cumple la condicion se ejecuta este bloque de codigo
+    console.log("Lleva paraguas");
+} else {
+    //si no se cumple la condicion se ejecuta este bloque de codigo
+    console.log("No lleva paraguas");
+}
