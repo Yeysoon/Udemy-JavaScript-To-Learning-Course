66bc0311049fbd41fea007e12de1a83653f13a40
Yeysoon <yeysonaldair13@gmail.com>
2026-01-15 23:16:59 -0600

    ventanas, alertas y promts

---
+++ Seccion 12/11-ventanas/main.js
@@ -0,0 +1,12 @@
+//Alertas
+let mensaje = "Hola soy Yeyson Barillas";
+alert(mensaje);
+
+
+//Ventanas de confirmacion
+let pregunta = confirm("¿Estas seguro de que quieres continuar?");
+console.log(pregunta);
+
+//Ventanas de prompt (Ingreso de datos)
+let nombre = prompt("¿Cual es tu nombre?", "Juanito Miguel");
+console.log(parseInt(typeof nombre));
