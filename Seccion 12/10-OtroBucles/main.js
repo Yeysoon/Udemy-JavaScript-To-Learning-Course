2b5a2a9149746732f53c9cd04a09ff04b5e53998
Yeysoon <yeysonaldair13@gmail.com>
2026-01-15 23:02:06 -0600

    bucle while

---
+++ Seccion 12/10-OtroBucles/main.js
@@ -0,0 +1,21 @@
+/*
+    Bucle while
+    
+    while (condicion) {
+        codigo que se ejecutara mientras se cumpla la condicion
+    }
+    Ejemplo:
+*/
+
+let year = 1990;
+let objetivo = 2177;
+
+while (year != 2177) {
+    console.log("Estamos en el año: " + year);
+
+
+    if (year == 2117) {
+        break;
+    }
+    year++;
+}   
