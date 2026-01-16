d3e597139e1b424f0929a99c89525daae11559e1
Yeysoon <yeysonaldair13@gmail.com>
2026-01-15 22:32:25 -0600

    bucle for

---
+++ Seccion 12/09-Bucles/main.js
@@ -0,0 +1,20 @@
+/*Bucle for = es una estructura de control que ejecuta un bloque de codigo varias veces de forma
+automatica mientras que se cumpla una condicion
+
+estructura:
+
+for (inicializacion; condicion; incremento){
+    codigo que se ejecutara mientras se cumpla la condicion
+}
+
+Ejemplo:
+
+
+*/
+
+
+let numero = 177;
+
+for (let contador = 0; contador <= numero; contador++) {
+    console.log(contador);
+}
